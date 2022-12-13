46405796ce653f708c3d0289c6f535fbb4e4f25e
dianaseung <diana.seung@liferay.com>
2022-12-13 13:37:22 -0800

    74 - refactor

--- dxp_setup.sh
+++ dxp_setup.sh
@@ -15,7 +15,7 @@ select version in "${DXP[@]}"; do
     case $version in
         "7.4")
             echo "Ok, DXP $version was selected"
-            # optionally call a function or run some code here
+            
             # CHOOSE A FIX PACK / UPDATE
             read -p 'Select DXP 7.4 patch level (Update): ' update
             until [[ $update =~ ^[+]?[0-9]+$ ]]
@@ -29,65 +29,65 @@ select version in "${DXP[@]}"; do
 
             # NAME THE PROJECT
             read -p 'Project Code: ' project
-
-            read -p 'Is this adding to an existing project?' addon
+            echo
+            
+            # ADD-ON?
+            echo "Is this adding to an existing project?"
             addon=('no' 'yes')
             select addonchoice in "${addon[@]}"; do
                 case $addonchoice in
-                "no")
-                    \cp -r /home/${USER}/Downloads/Liferay/DXP/7.4/liferay-dxp-tomcat-7.4.13.u$update /home/${USER}/Downloads/Liferay/PROJECTS/$project
-                    \cp /home/${USER}/Downloads/Liferay/DXP/Keys/7.4.xml /home/${USER}/Downloads/Liferay/PROJECTS/$project/liferay-dxp-7.4.13.u$update/deploy/
-                    \cp /home/${USER}/Downloads/Liferay/DXP/portal-ext.properties /home/${USER}/Downloads/Liferay/PROJECTS/$project/liferay-dxp-7.4.13.u$update/
-                    echo
-                    # MAKE THE MYSQL SCHEMA
-                    if [ -f /root/.my.cnf ]; then
-                        mysql -e "CREATE SCHEMA 74_$project;"
-                    else
-                        # echo "Enter root user MySQL pw:"
-                        # read -sp 'Enter root user MySQL pw: ' rootpw
-                        rootpw = $MYSQL_PW
-                        mysql -uroot -p${rootpw} -e "CREATE SCHEMA 74_${project}"
-                    fi
-                    echo "Database schema 74_${project} made!"
-                    sed -i "s/SCHEMA/$project/g" /home/${USER}/Downloads/Liferay/PROJECTS/$project/liferay-dxp-7.4.13.u$update/portal-ext.properties
-                    echo "portal-ext.properties updated with newly made schema!"
-                    echo "---"
-                    echo
-                    echo "Success -- Finished setup of DXP 7.4 u$update folder for $project"
-                    xdg-open /home/${USER}/Downloads/Liferay/PROJECTS/$project
-                    break
-                    ;;
-                "yes")
-                    \cp -r /home/${USER}/Downloads/Liferay/DXP/7.4/liferay-dxp-tomcat-7.4.13.u$update/liferay-dxp-tomcat-7.4.13.u$update /home/${USER}/Downloads/Liferay/PROJECTS/$project//liferay-dxp-tomcat-7.4.13.u$update_${DATE}
-                    \cp /home/${USER}/Downloads/Liferay/DXP/Keys/7.4.xml /home/${USER}/Downloads/Liferay/PROJECTS/$project/liferay-dxp-7.4.13.u$update_${DATE}/deploy/
-                    \cp /home/${USER}/Downloads/Liferay/DXP/portal-ext.properties /home/${USER}/Downloads/Liferay/PROJECTS/$project/liferay-dxp-7.4.13.u$update_${DATE}/
-                    echo
-                    # MAKE THE MYSQL SCHEMA
-                    if [ -f /root/.my.cnf ]; then
-                        mysql -e "CREATE SCHEMA 74_$project;"
-                    else
-                        # echo "Enter root user MySQL pw:"
-                        # read -sp 'Enter root user MySQL pw: ' rootpw
-                        rootpw = $MYSQL_PW
-                        mysql -uroot -p${rootpw} -e "CREATE SCHEMA 74_${project}"
-                    fi
-                    echo "Database schema 74_${project} made!"
-                    sed -i "s/SCHEMA/$project/g" /home/${USER}/Downloads/Liferay/PROJECTS/$project/liferay-dxp-7.4.13.u$update_${DATE}/portal-ext.properties
-                    echo "portal-ext.properties updated with newly made schema!"
-                    echo "---"
-                    echo
-                    echo "Success -- Finished setup of DXP 7.4 u$update folder for $project"
-                    xdg-open /home/${USER}/Downloads/Liferay/PROJECTS/$project
-                    break
-                    ;;
-
-                "cancel")
-                    echo "canceling..."
-                    break
-                    ;;
+                    "no")
+                        \cp -r /home/${USER}/Downloads/Liferay/DXP/7.4/liferay-dxp-tomcat-7.4.13.u$update /home/${USER}/Downloads/Liferay/PROJECTS/$project
+                        \cp /home/${USER}/Downloads/Liferay/DXP/Keys/7.4.xml /home/${USER}/Downloads/Liferay/PROJECTS/$project/liferay-dxp-7.4.13.u$update/deploy/
+                        \cp /home/${USER}/Downloads/Liferay/DXP/portal-ext.properties /home/${USER}/Downloads/Liferay/PROJECTS/$project/liferay-dxp-7.4.13.u$update/
+                        echo
+                        # MAKE THE MYSQL SCHEMA
+                        if [ -f /root/.my.cnf ]; then
+                            mysql -e "CREATE SCHEMA 74_$project;"
+                        else
+                            # echo "Enter root user MySQL pw:"
+                            # read -sp 'Enter root user MySQL pw: ' rootpw
+                            rootpw = $MYSQL_PW
+                            mysql -uroot -p${rootpw} -e "CREATE SCHEMA 74_${project}"
+                        fi
+                        echo "Database schema 74_${project} made!"
+                        sed -i "s/SCHEMA/$project/g" /home/${USER}/Downloads/Liferay/PROJECTS/$project/liferay-dxp-7.4.13.u$update/portal-ext.properties
+                        echo "portal-ext.properties updated with newly made schema!"
+                        echo "---"
+                        echo
+                        echo "Success -- Finished setup of DXP 7.4 u$update folder for $project"
+                        xdg-open /home/${USER}/Downloads/Liferay/PROJECTS/$project
+                        break
+                        ;;
+                    "yes")
+                        \cp -r /home/${USER}/Downloads/Liferay/DXP/7.4/liferay-dxp-tomcat-7.4.13.u$update/liferay-dxp-tomcat-7.4.13.u$update /home/${USER}/Downloads/Liferay/PROJECTS/$project//liferay-dxp-tomcat-7.4.13.u$update_${DATE}
+                        \cp /home/${USER}/Downloads/Liferay/DXP/Keys/7.4.xml /home/${USER}/Downloads/Liferay/PROJECTS/$project/liferay-dxp-7.4.13.u$update_${DATE}/deploy/
+                        \cp /home/${USER}/Downloads/Liferay/DXP/portal-ext.properties /home/${USER}/Downloads/Liferay/PROJECTS/$project/liferay-dxp-7.4.13.u$update_${DATE}/
+                        echo
+                        # MAKE THE MYSQL SCHEMA
+                        if [ -f /root/.my.cnf ]; then
+                            mysql -e "CREATE SCHEMA 74_$project;"
+                        else
+                            # echo "Enter root user MySQL pw:"
+                            # read -sp 'Enter root user MySQL pw: ' rootpw
+                            rootpw = $MYSQL_PW
+                            mysql -uroot -p${rootpw} -e "CREATE SCHEMA 74_${project}"
+                        fi
+                        echo "Database schema 74_${project} made!"
+                        sed -i "s/SCHEMA/$project/g" /home/${USER}/Downloads/Liferay/PROJECTS/$project/liferay-dxp-7.4.13.u$update_${DATE}/portal-ext.properties
+                        echo "portal-ext.properties updated with newly made schema!"
+                        echo "---"
+                        echo
+                        echo "Success -- Finished setup of DXP 7.4 u$update folder for $project"
+                        xdg-open /home/${USER}/Downloads/Liferay/PROJECTS/$project
+                        break
+                        ;;
+                    "cancel")
+                        echo "canceling..."
+                        break
+                        ;;
                 esac
             done
-
         "Quit")
             echo "User requested exit"
             exit
