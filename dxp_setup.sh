bbef729fe38b90126ab6a9bd01c2bbf495d0abe8
dianaseung <diana.seung@liferay.com>
2023-05-11 17:57:14 -0700

    feat: global var checks

--- dxp_setup.sh
+++ dxp_setup.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 # Project: Quick LR - Liferay DXP setup automation for Support CSE
 DATE=$(date +%y%m%d%H%M)
-# GLOBAL VARIABLE CHECKS
+# CHECK: GLOBAL VARIABLES (set in bashrc)
 if [ -z ${LRDIR+x} ]; then
     echo "WARN: Please set LRDIR in ~/.bashrc first!"
     exit 1
 else
-    echo "CHECK: LRDIR is set to ${LRDIR}"
+    echo "CHECK: LRDIR is ${LRDIR}"
 fi
 if [ -z ${PROJECTDIR+x} ]; then
     echo "WARN: Please set PROJECTDIR in ~/.bashrc first!"
     exit 1
 else
-    echo "CHECK: PROJECTDIR is set to ${PROJECTDIR}"
+    echo "CHECK: PROJECTDIR is ${PROJECTDIR}"
 fi
 if [ -z ${MYSQLUSER+x} ]; then
     echo "WARN: Please set MYSQLUSER in ~/.bashrc first!"
     exit 1
 else
-    echo "CHECK: MYSQLUSER is set to ${MYSQLUSER}"
+    echo "CHECK: MYSQLUSER is ${MYSQLUSER}"
 fi
 
-# TODO: CHECK THE MYSL DB VERSION NEEDED
+# CHECK: MYSL DB VERSION/PORT
 MYSQLPORTLN=`grep 'jdbc.default.url' ${LRDIR}/portal-ext.properties`
 propPrefix='jdbc.default.url=jdbc:mysql://localhost:'
 propSuffix='?characterEncoding=UTF-8&dontTrackOpenResources=true&holdResultsOpenOverStatementClose=true&serverTimezone=GMT&useFastDateParsing=false&useUnicode=true'
 dbNameA=${MYSQLPORTLN/$propPrefix/}
 dbNameB=${dbNameA/$propSuffix/}
 dbPort=${dbNameB%/*}
-echo "CHECK: Current MYSQL Port: $dbPort"
+echo "CHECK: Current MYSQL Port is $dbPort"
+echo -e "\n---"
 
-# NAME THE PROJECT
+# NAME THE PROJECT DIR
 read -p 'Project Code: ' project
 mkdir -p "${PROJECTDIR}"/"$project"/
 if [[ -e "${PROJECTDIR}"/"$project"/ ]]; then
@@ -40,15 +41,15 @@ else
     xdg-open "${PROJECTDIR}"
 fi
 
-                
+# FUNCTIONS
 checkDir () {
     # CHECK IF BUNDLE EXISTS ALREADY - append date if so
-    if [ -d "${PROJECTDIR}/${project}/${BUNDLED}" ]; then
+    if [ -d "${PROJECTDIR}"/"${project}"/"${BUNDLED}" ]; then
         BUNDLED="${BUNDLED}.${DATE}"
         SCHEMA="${SCHEMA}_${DATE}"
-        echo "Project $project with $1 $update folder already exists! Appending date..."
+        echo "Project $project with "$1" "$update" folder already exists! Appending date..."
     else
-        echo "Project $project with $1 $update folder does not exist yet..."
+        echo "Project $project with "$1" "$update" folder does not exist yet!"
     fi
 }
 
@@ -73,16 +74,8 @@ updatePatchingTool () {
     fi
 }
 
-updatePortalExtDB () {
-    # UPDATE PORTAL-EXT WITH NEW DB
-    sed -i "s/SCHEMA/$SCHEMA/g" ${PROJECTDIR}/$project/$BUNDLED/portal-ext.properties
-    echo -e "\tSUCCESS: Updated portal-ext.properties with $SCHEMA"
-}
-
 createDB () {
-    # MAKE THE MYSQL SCHEMA
     mysql -u$MYSQLUSER -e "CREATE SCHEMA ${SCHEMA}";
-    # echo -e "mysql -u${MYSQLUSER} -e "CREATE SCHEMA ${SCHEMA}";"
     CHECKDB=`mysql -e "SHOW DATABASES" | grep $SCHEMA`
     if [ $CHECKDB == $SCHEMA ]; then
         echo -e "\tSUCCESS: Created database ${SCHEMA}"
@@ -91,10 +84,14 @@ createDB () {
     fi
 }
 
+updatePortalExtDB () {
+    sed -i "s/SCHEMA/$SCHEMA/g" ${PROJECTDIR}/$project/$BUNDLED/portal-ext.properties
+    echo -e "\tSUCCESS: Updated portal-ext.properties with $SCHEMA"
+}
+
 patchInstall () {
     # COPY FP + PATCH + CLEAN TEMP FILES
     FPZIP="liferay-fix-pack-dxp-$update-$versiontrim.zip"
-    # echo -e "\tDEBUG: Fix Pack sourced from ${LRDIR}/$FPZIP"
     cp ${LRDIR}/$version/FP/$FPZIP ${PROJECTDIR}/$project/$BUNDLED/patching-tool/patches/
     # If FP copied properly, then install 
     if [ -e ${PROJECTDIR}/$project/$BUNDLED/patching-tool/patches/$FPZIP ]; then
@@ -113,7 +110,7 @@ createBundle () {
     checkDir $1
     # CREATE FOLDER
     cp -r ${LRDIR}/$SRC ${PROJECTDIR}/$project/$BUNDLED
-    echo -e "\tSUCCESS: DXP $version $1 $update folder created at ${PROJECTDIR}/$project/$BUNDLED"
+    echo -e "\tSUCCESS: DXP $version $1 $update folder created at $project/$BUNDLED"
     if [ -d "${PROJECTDIR}/$project/$BUNDLED/" ]; then
         # Install Portal-Ext - Always Needed
         cp ${LRDIR}/portal-ext.properties ${PROJECTDIR}/$project/$BUNDLED/
@@ -159,15 +156,14 @@ select version in "${DXP[@]}"; do
     case $version in
         "7.4.13")
             versiontrimx=${versiontrim//13}
-            read -p "Select DXP $version patch level (Update): " update
+            read -p "Select DXP $version patch level: " update
             numcheck='^[0-9]+$'
             until [[ $update =~ ($numcheck|master) ]]; do
-                echo -e "\tERROR: Invalid Input. Valid Inputs: Update # or master.\n"
+                echo -e "\tERROR: Invalid Input. Valid Inputs: Update or master.\n"
                 read -p "Select DXP $version patch level (Update): " update
             done
             echo -e "\n---\n"
             if [ $update == 'master' ]; then
-                # TODO: Need to update master
                 SRC="Branch/liferay-portal-tomcat-master-all/liferay-portal-master-all"
                 BUNDLED="liferay-dxp-$version-$update"
                 SCHEMA="${versiontrimx}_${project}_$update"
@@ -183,36 +179,31 @@ select version in "${DXP[@]}"; do
         "7.3.10")
             # START 73 - USES UPDATES, SP and FP
             versiontrimx=${versiontrim//10}
-            read -p "Select DXP $version patch level (Update or FP #): " update
+            read -p "Select DXP $version patch level: " update
             numcheck='^[0-9]+$'
             until [[ $update =~ ($numcheck|branch) ]]; do
-                echo -e "ERROR: Invalid Input. Valid Inputs: Update/FP # or branch.\n"
-                read -p "Select DXP $version patch level (Update or FP #): " update
+                echo -e "ERROR: Invalid Input. Valid Inputs: Update, FP or branch.\n"
+                read -p "Select DXP $version patch level: " update
             done
             echo -e "\n---\n"
-            # TODO: Potential Refactor
             if (( $update > 3 )); then
-                # -- IF UPDATE
                 SRC="$version/liferay-dxp-tomcat-$version.u$update/liferay-dxp-$version.u$update"
                 BUNDLED="liferay-dxp-$version.u$update"
                 SCHEMA="${versiontrimx}_${project}_U${update}"
                 createBundle Update
             elif (( $update == 1 )) || (( $update == 3 )); then
                 # -- IF SP
-                # echo "Patch level is an SP: sp$update"
                 SRC="$version/liferay-dxp-tomcat-$version-sp$update/liferay-dxp-$version.$update-sp$update"
                 BUNDLED="liferay-dxp-$version-sp$update"
                 SCHEMA="${versiontrimx}_${project}_SP${update}"
-                createBundle Update
+                createBundle SP
             elif [ $update == 'branch' ]; then
-                # -- IF 73 BRANCH
                 versiontrim=${version//.10}
                 SRC="Branch/liferay-portal-tomcat-${versiontrim}.x-private-all/liferay-portal-${versiontrim}.x-private-all"
                 BUNDLED="liferay-$version-$update"
                 SCHEMA="${versiontrimx}_${project}_$update"
                 createBundle Branch
             else
-                # -- IF FP
                 SRC="$version/liferay-dxp-tomcat-$version-ga1/liferay-dxp-$version-ga1"
                 BUNDLED="liferay-dxp-$version.dxp-$update"
                 SCHEMA="${versiontrimx}_${project}_dxp${update}"
@@ -225,20 +216,18 @@ select version in "${DXP[@]}"; do
             read -p "Select DXP $version patch level: " update
             numcheck='^[0-9]+$'
             until [[ $update =~ ($numcheck|branch) ]]; do
-                echo -e "ERROR: Invalid Input. Valid Inputs: Update/FP # or branch\n"
+                echo -e "ERROR: Invalid Input. Valid Inputs: Update, FP or branch\n"
                 read -p "Select DXP $version patch level: " update
             done
             echo -e "\n---\n"
 
             if [ $update == 'branch' ]; then
                 versiontrim=${version//.10}
-                # -- IF 70-72 BRANCH
                 SRC="Branch/liferay-portal-tomcat-$versiontrim.x-private-all/liferay-portal-$versiontrim.x-private-all"
                 BUNDLED="liferay-$versiontrim-$update"
                 SCHEMA="${versiontrimx}_${project}_$update"
                 createBundle Branch
             else
-                # -- IF FP
                 SRC="$version/liferay-dxp-tomcat-$version-ga1/liferay-dxp-$version-ga1"
                 BUNDLED="liferay-dxp-$version.dxp-$update"
                 SCHEMA="${versiontrimx}_${project}_dxp${update}"
@@ -253,31 +242,26 @@ select version in "${DXP[@]}"; do
             numcheck='^[0-9]+$'
             until [[ $update =~ ($numcheck|branch) ]]; do
                 echo -e "\tERROR: Invalid Input. Valid Inputs: SP # or branch\n"
-                read -p "Select Portal $version patch level (SP #): " update
+                read -p "Select Portal $version patch level: " update
             done
             echo -e "\n---\n"
             # TODO: consider hashmap / lookup table to make FP to SP
             # declare -A fixpacks
             # fixpacks=( ["SP20"]=154 ["SP19"]=148 ["SP18"]=138)
-
             if (( $update > 20 )); then
                 echo -e "\tWARN: Service Pack needed, no fix pack support yet."
             elif [ $update == 'branch' ]; then
-                # -- IF 62 61 BRANCH
                 echo -e "\tWARN: No branch support yet for Portal 6.2 or 6.1"
             else
-                # -- IF SP
                 echo "Ok, setting up a $project folder with Portal $version SP $update bundle..."
                 SRC="$version/liferay-portal-tomcat-$version-ee-sp$update/liferay-portal-$version-ee-sp$update"
                 BUNDLED="liferay-portal-tomcat-$version.ee-sp$update"
                 SCHEMA="${versiontrim}_${project}_SP${update}"
-                createBundle Update
+                createBundle SP
             fi
             break
             ;;
         "Config")
-            DBDserver_DIR=$DBDEPLOYER_HOME/servers/$mysqlserver/
-            echo "The DBDeployer server dir is $DBDserver_DIR"
             echo "DBDeployer versions command:"
             dbdeployer versions
             # Send list of installed DBDeployer servers to .txt
@@ -285,13 +269,10 @@ select version in "${DXP[@]}"; do
             mysqlserverlist=mysqlserverlist.txt
             ls "${DBDEPLOYER_HOME}/servers/" > $mysqlserverlist
             # Check if 3306 in server list, if not add
-            # FIRE TODO: fix grep
             if grep -Fxq "3306" $mysqlserverlist
                 then
-                    # code if found -- NOTHING
                     echo -e "\tCHECK: 3306 already in $mysqlserverlist"
                 else
-                    # code if not found
                     echo -e "\tCHECK: 3306 not yet in $mysqlserverlist -- inserting 3306 as an option"
                     echo -e "3306" >> $mysqlserverlist
                 fi
@@ -299,8 +280,10 @@ select version in "${DXP[@]}"; do
             mysqlarray=(`cat "$mysqlserverlist"`)
 
             select mysqlserver in "${mysqlarray[@]}"; do
+                DBDserver_DIR=$DBDEPLOYER_HOME/servers/$mysqlserver/
+                # echo -e "\tThe DBDeployer server dir is $DBDserver_DIR"
                 # UPDATE portal-ext with selected server
-                sed -i "s!localhost:*/SCHEMA!$mysqlserver!g" ${LRDIR}/portal-ext.properties
+                sed -i "s!localhost:.*/SCHEMA!localhost:$mysqlserver/SCHEMA!g" ${LRDIR}/portal-ext.properties
                 echo -e "\tSUCCESS: Master portal-ext.properties updated! MySQL Server set at localhost:${mysqlserver}\n---\n"
                 if [ $mysqlserver == '3306' ]; then
                     # This is on by default if mysql installed
@@ -323,7 +306,7 @@ select version in "${DXP[@]}"; do
     esac
 done
 
-echo -e "\tSUCCESS: Finished setup of ${PROJECTDIR}/$project/$BUNDLED"
+echo -e "\tSUCCESS: Completed setup of $project/$BUNDLED"
 echo -e "\n---\n"
 # START BUNDLE OR EXIT SCRIPT
 read -rsn1 -p"Press any key to start $BUNDLED bundle... or Ctrl-C to exit";echo
